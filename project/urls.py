ac0c4e3c63ecc28b7195dc6c4e5c321ede1ea6d0
田端佑也 <tabayu5@gmail.com>
2020-07-11 23:31:03 +0900

    add urls

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authen/', views.obtain_auth_token),
+    path('api/user/', include('api_user.urls')),
+    path('api/dm/', include('api_dm.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
