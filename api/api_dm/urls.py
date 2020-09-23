3cb6d9f5f89aa70ed3a7a13cfbfd9d5430c8da35
田端佑也 <tabayu5@gmail.com>
2020-09-23 22:12:49 +0900

    create pj&apps

---
+++ api/api_dm/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+app_name = 'dm'
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('',include(router.urls))
+]
+
---
+++ api/api_user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+app_name = 'user'
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('',include(router.urls))
+]
+
