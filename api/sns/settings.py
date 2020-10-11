ee4f768e85fd16da38cd782442ff2f7f60afb474
田端佑也 <tabayu5@gmail.com>
2020-10-11 23:05:20 +0900

    add Login

--- api/sns/settings.py
+++ api/sns/settings.py
@@ -61,7 +61,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000"
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8080",
+    "http://localhost:3000",
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = 'sns.urls'
